72618a1faef9ace8b3908e8fbc5fcb873b618b1d
ruanchangming <ruanchangming@lianjia.com>
2018-01-22 18:39:04 +0800

    commit realm with error

--- shiro-core-demo/src/test/java/com/roytrack/shiro/LoginLogoutTest.java
+++ shiro-core-demo/src/test/java/com/roytrack/shiro/LoginLogoutTest.java
@@ -4,6 +4,7 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.RealmSecurityManager;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Factory;
@@ -42,6 +43,33 @@ public class LoginLogoutTest {
         subject.logout();
 
 
+
+    }
+
+    @Test
+    public void testRealm(){
+        //获取SecurityManage工厂 用ini初始化
+        Factory<SecurityManager> factory=new RealmSecurityManager("classpath:shiro-realm.ini");
+        SecurityManager securityManager =factory.getInstance();
+
+
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject subject=SecurityUtils.getSubject();
+
+        UsernamePasswordToken token=new UsernamePasswordToken("roy","123");
+
+        try{
+            subject.login(token);
+        }catch (AuthenticationException e){
+            e.printStackTrace();
+            System.out.println("登录失败");
+        }
+
+
+        Assert.assertEquals(true,subject.isAuthenticated());
+
+        subject.logout();
 
     }
 }
